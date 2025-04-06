c2c4e452094df132379fe3c23cf4d7dc617e97fe
Sohan Rout <sohanrout84@gmail.com>
2025-04-06 19:31:09 +0530

    added metadata

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,10 +1,18 @@
 import "./globals.css";
 
 export const metadata = {
-  title: "DsaVisualizer",
-  description: "Learn DSA with the help of illustrations.",
-};
-
+  title: 'DSA Visualizer | Learn DSA Easily',
+  description: 'Visualize Data Structures like Stack, Queue, Trees, Graphs and learn DSA interactively.',
+  keywords: ['DSA Visualizer', 'Algorithms', 'Learn DSA', 'Data Structures', 'Visualize Algorithms'],
+  authors: [{ name: 'Sohan Rout' }],
+  robots: 'index, follow',
+  openGraph: {
+    title: 'DSA Visualizer',
+    description: 'Best platform to learn DSA visually.',
+    url: 'https://dsa-visualizer-sigma.vercel.app/',
+    siteName: 'DSA Visualizer',
+  },
+}
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
