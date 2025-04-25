e442380d56c320e6084516e29fbaead79e04acbb
Sohan Rout <sohanrout84@gmail.com>
2025-04-25 20:53:51 +0530

    added padding

--- app/visualizer/sorting/selectionsort/animation.jsx
+++ app/visualizer/sorting/selectionsort/animation.jsx
@@ -157,7 +157,7 @@ const SelectionSortVisualizer = () => {
                   </button>
                   <button
                     onClick={reset}
-                    className="w-full bg-red-600 hover:bg-red-700 text-white px-4 py-2 rounded"
+                    className="w-full bg-red-600 hover:bg-red-700 text-white mt-4 px-4 py-2 rounded"
                   >
                     Reset All
                   </button>
