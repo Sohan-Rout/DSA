8e8fcaee89821ee76fafb7673f3a9fe80b04808a
Sohan Rout <sohanrout84@gmail.com>
2025-05-10 12:05:33 +0530

    updated the list of conetent

--- app/visualizer/page.jsx
+++ app/visualizer/page.jsx
@@ -5,7 +5,7 @@ import VisualizerClient from './VisualizerClient';
 
 export const metadata = {
   title: 'Algorithm Visualizer | DSA Visualizer',
-  description: 'Explore visual representations and source code for various DSA algorithms including searching, sorting, stacks, queues, trees, and stack-based expression evaluation like Polish Notation using arrays and linked lists. Interactive and beginner-friendly!',
+  description: 'Explore visual representations and source code for various DSA algorithms including searching, sorting, stacks, queues, trees, graphs, and stack-based expression evaluation like Polish Notation using arrays and linked lists. Interactive and beginner-friendly!',
   keywords: [
     'DSA Visualizer',
     'Algorithm Visualizer',
@@ -15,6 +15,10 @@ export const metadata = {
     'Stack',
     'Queue',
     'Tree',
+    'Graph',
+    'Graph Algorithms',
+    'BFS',
+    'DFS',
     'Linear Search',
     'Bubble Sort',
     'Tree Traversal',
@@ -37,6 +41,7 @@ export const metadata = {
     'Priority Queue',
     'Deque',
     'Queue Operations',
+    'Graph Traversal',
     'Code for DSA Algorithms',
     'Code for Data Structures',
     'Interactive Code Samples',
@@ -45,157 +50,418 @@ export const metadata = {
   robots: 'index, follow',
 };
 
-  const sections = [
-    {
-      title: 'Array',
-      icon: (
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" />
-        </svg>
-      ),
-      subsections: [
-        {
-          title: 'Searching',
-          items: [
-            { name: 'Linear Search', path: '/visualizer/searching/linearsearch' },
-            { name: 'Binary Search', path: '/visualizer/searching/binarysearch' },
-          ],
-        },
-        {
-          title: 'Sorting',
-          items: [
-            { name: 'Bubble Sort', path: '/visualizer/sorting/bubblesort' },
-            { name: 'Selection Sort', path: '/visualizer/sorting/selectionsort' },
-            { name: 'Insertion Sort', path: '/visualizer/sorting/insertionsort' },
-            { name: 'Merge Sort', path: '/visualizer/sorting/mergesort' },
-            { name: 'Quick Sort', path: '/visualizer/sorting/quicksort' },
-          ],
-        },
-      ],
-    },
-    {
-      title: 'Stack',
-      icon: (
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 12h14M5 12a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v4a2 2 0 01-2 2M5 12a2 2 0 00-2 2v4a2 2 0 002 2h14a2 2 0 002-2v-4a2 2 0 00-2-2m-2-4h.01M17 16h.01" />
-        </svg>
-      ),
-      subsections: [
-        {
-          title: 'Operations',
-          items: [
-            { name: 'Push & Pop', path: '/visualizer/stack/push-pop' },
-            { name: 'Peek', path: '/visualizer/stack/peek' },
-            { name: 'Is Empty', path: '/visualizer/stack/isempty' },
-            { name: 'Is Full', path: '/visualizer/stack/isfull' },
-          ],
-        },
-        {
-          title: 'Polish Notations Evaluation',
-          items: [
-            { name: 'Postfix', path: '/visualizer/stack/polish/postfix' },
-            { name: 'Prefix', path: '/visualizer/stack/polish/prefix' },
-          ],
-        },
-        {
-          title: 'Implementation',
-          items: [
-            { name: 'Using Array', path: '/visualizer/stack/implementation/usingArray' },
-            { name: 'Using Linked List', path: '/visualizer/stack/implementation/usingLinkedList' },
-          ],
-        },
-      ],
-    },
-    {
-      title: 'Queue',
-      icon: (
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16m-7 6h7" />
-        </svg>
-      ),
-      subsections: [
-        {
-          title: 'Operations',
-          items: [
-            { name: 'Enqueue & Dequeue', path: '/visualizer/queue/operations/enqueue-dequeue' },
-            { name: 'Peek Front', path: '/visualizer/queue/operations/peek-front' },
-            { name: 'Is Empty', path: '/visualizer/queue/operations/isempty' },
-            { name: 'Is Full', path: '/visualizer/queue/operations/isfull' },
-          ]
-        },
-        {
-          title: 'Types',
-          items: [
-            { name: 'Single Ended Queue', path: '/visualizer/queue/types/singleEnded' },
-            { name: 'Double Ended Queue (Deque)', path: '/visualizer/queue/types/deque' },
-            { name: 'Circular Queue', path: '/visualizer/queue/types/circular' },
-            { name: 'Priority Queue', path: '/visualizer/queue/types/priority' },
-            { name: 'Multiple Queue', path: '/visualizer/queue/types/multiple' },
-          ]
-        },
-        {
-          title: 'Implementation',
-          items: [
-            { name: 'Using Array', path: '/visualizer/queue/implementation/array' },
-            { name: 'Using Linked List', path: '/visualizer/queue/implementation/linkedList' },
-          ]
-        }
-      ],
-    },
-    {
-      title: 'Linked List',
-      icon: (
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 5l7 7-7 7" />
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 5l7 7-7 7" />
-        </svg>
-      ),
-      subsections: [
-        {
-          title: 'Types',
-          items: [
-            { name: 'Singly Linked List', path: '/visualizer/linkedList/types/singly' },
-            { name: 'Doubly Linked List', path: '/visualizer/linkedList/types/doubly' },
-            { name: 'Circular Linked List', path: '/visualizer/linkedList/types/circular' },
-          ],
-        },
-        {
-          title: 'Operations',
-          items: [
-            { name: 'Insertion', path: '/visualizer/linkedlist/operations/insertion' },
-            { name: 'Deletion', path: '/visualizer/linkedlist/operations/deletion' },
-            { name: 'Finding an Element', path: '/visualizer/linkedlist/operations/search' },
-            { name: 'Merging Two Lists', path: '/visualizer/linkedlist/operations/merge' },
-            { name: 'Comparison', path: '/visualizer/linkedlist/operations/comparison' },
-          ],
-        },
-      ],
-    },
-    {
-      title: 'Tree',
-      icon: (
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 12h6m-6 4h6m0-8h6m6 0h-6m6 4h-6m-6-8v12m6-12v12" />
-        </svg>
-      ),
-      subsections: [
-        {
-          title: 'Traversal',
-          items: [
-            { name: 'Pre-order Traversal', path: '/visualizer/trees/traversing/pre-order' },
-            { name: 'In-order Traversal', path: '/visualizer/trees/traversing/in-order' },
-            { name: 'Post-order Traversal', path: '/visualizer/trees/traversing/post-order' },
-          ],
-        },
-        {
-          title: 'Sorting',
-          items: [
-            { name: 'Heap Sort', path: '/visualizer/trees/sorting/heapsort' },
-          ],
-        },
-      ],
-    },
-  ];
+const sections = [
+  {
+    title: "Array",
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M9 5l7 7-7 7"
+        />
+      </svg>
+    ),
+    subsections: [
+      {
+        title: "Searching",
+        items: [
+          { name: "Linear Search", path: "/visualizer/searching/linearsearch" },
+          { name: "Binary Search", path: "/visualizer/searching/binarysearch" },
+        ],
+      },
+      {
+        title: "Sorting",
+        items: [
+          { name: "Bubble Sort", path: "/visualizer/sorting/bubblesort" },
+          { name: "Selection Sort", path: "/visualizer/sorting/selectionsort" },
+          { name: "Insertion Sort", path: "/visualizer/sorting/insertionsort" },
+          { name: "Merge Sort", path: "/visualizer/sorting/mergesort" },
+          { name: "Quick Sort", path: "/visualizer/sorting/quicksort" },
+        ],
+      },
+    ],
+  },
+  {
+    title: "Stack",
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M5 12h14M5 12a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v4a2 2 0 01-2 2M5 12a2 2 0 00-2 2v4a2 2 0 002 2h14a2 2 0 002-2v-4a2 2 0 00-2-2m-2-4h.01M17 16h.01"
+        />
+      </svg>
+    ),
+    subsections: [
+      {
+        title: "Operations",
+        items: [
+          { name: "Push & Pop", path: "/visualizer/stack/push-pop" },
+          { name: "Peek", path: "/visualizer/stack/peek" },
+          { name: "Is Empty", path: "/visualizer/stack/isempty" },
+          { name: "Is Full", path: "/visualizer/stack/isfull" },
+        ],
+      },
+      {
+        title: "Polish Notations Evaluation",
+        items: [
+          { name: "Postfix", path: "/visualizer/stack/polish/postfix" },
+          { name: "Prefix", path: "/visualizer/stack/polish/prefix" },
+        ],
+      },
+      {
+        title: "Implementation",
+        items: [
+          {
+            name: "Using Array",
+            path: "/visualizer/stack/implementation/usingArray",
+          },
+          {
+            name: "Using Linked List",
+            path: "/visualizer/stack/implementation/usingLinkedList",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    title: "Queue",
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M4 6h16M4 12h16m-7 6h7"
+        />
+      </svg>
+    ),
+    subsections: [
+      {
+        title: "Operations",
+        items: [
+          {
+            name: "Enqueue & Dequeue",
+            path: "/visualizer/queue/operations/enqueue-dequeue",
+          },
+          {
+            name: "Peek Front",
+            path: "/visualizer/queue/operations/peek-front",
+          },
+          { name: "Is Empty", path: "/visualizer/queue/operations/isempty" },
+          { name: "Is Full", path: "/visualizer/queue/operations/isfull" },
+        ],
+      },
+      {
+        title: "Types",
+        items: [
+          {
+            name: "Single Ended Queue",
+            path: "/visualizer/queue/types/singleEnded",
+          },
+          {
+            name: "Double Ended Queue (Deque)",
+            path: "/visualizer/queue/types/deque",
+          },
+          { name: "Circular Queue", path: "/visualizer/queue/types/circular" },
+          { name: "Priority Queue", path: "/visualizer/queue/types/priority" },
+          { name: "Multiple Queue", path: "/visualizer/queue/types/multiple" },
+        ],
+      },
+      {
+        title: "Implementation",
+        items: [
+          {
+            name: "Using Array",
+            path: "/visualizer/queue/implementation/array",
+          },
+          {
+            name: "Using Linked List",
+            path: "/visualizer/queue/implementation/linkedList",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    title: "Linked List",
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M13 5l7 7-7 7"
+        />
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M5 5l7 7-7 7"
+        />
+      </svg>
+    ),
+    subsections: [
+      {
+        title: "Types",
+        items: [
+          {
+            name: "Singly Linked List",
+            path: "/visualizer/linkedList/types/singly",
+          },
+          {
+            name: "Doubly Linked List",
+            path: "/visualizer/linkedList/types/doubly",
+          },
+          {
+            name: "Circular Linked List",
+            path: "/visualizer/linkedList/types/circular",
+          },
+        ],
+      },
+      {
+        title: "Operations",
+        items: [
+          {
+            name: "Insertion",
+            path: "/visualizer/linkedlist/operations/insertion",
+          },
+          {
+            name: "Deletion",
+            path: "/visualizer/linkedlist/operations/deletion",
+          },
+          {
+            name: "Finding an Element",
+            path: "/visualizer/linkedlist/operations/search",
+          },
+          {
+            name: "Merging Two Lists",
+            path: "/visualizer/linkedlist/operations/merge",
+          },
+          {
+            name: "Comparison",
+            path: "/visualizer/linkedlist/operations/comparison",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    title: "Tree",
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M3 12h6m-6 4h6m0-8h6m6 0h-6m6 4h-6m-6-8v12m6-12v12"
+        />
+      </svg>
+    ),
+    subsections: [
+      {
+        title: "Binary Tree",
+        items: [
+          {
+            name: "Structure & Properties",
+            path: "/visualizer/trees/binary-tree/properties",
+          },
+          {
+            name: "Types of Binary Trees",
+            path: "/visualizer/trees/binary-tree/types",
+          },
+        ],
+      },
+      {
+        title: "Binary Search Tree",
+        items: [
+          { name: "Insertion", path: "/visualizer/trees/bst/insertion" },
+          { name: "Deletion", path: "/visualizer/trees/bst/deletion" },
+          { name: "Searching", path: "/visualizer/trees/bst/searching" },
+          { name: "Balancing (AVL)", path: "/visualizer/trees/bst/avl" },
+        ],
+      },
+      {
+        title: "Traversal",
+        items: [
+          { name: "Pre-order", path: "/visualizer/trees/traversal/pre-order" },
+          { name: "In-order", path: "/visualizer/trees/traversal/in-order" },
+          {
+            name: "Post-order",
+            path: "/visualizer/trees/traversal/post-order",
+          },
+          {
+            name: "Level-order (BFS)",
+            path: "/visualizer/trees/traversal/level-order",
+          },
+          {
+            name: "Morris Traversal",
+            path: "/visualizer/trees/traversal/morris",
+          },
+        ],
+      },
+      {
+        title: "Advanced Trees",
+        items: [
+          {
+            name: "Red-Black Trees",
+            path: "/visualizer/trees/advanced/red-black",
+          },
+          { name: "B-Trees", path: "/visualizer/trees/advanced/b-trees" },
+          {
+            name: "Trie (Prefix Tree)",
+            path: "/visualizer/trees/advanced/trie",
+          },
+          { name: "Segment Trees", path: "/visualizer/trees/advanced/segment" },
+          { name: "Fenwick Trees", path: "/visualizer/trees/advanced/fenwick" },
+        ],
+      },
+      {
+        title: "Algorithms",
+        items: [
+          {
+            name: "Lowest Common Ancestor",
+            path: "/visualizer/trees/algorithms/lca",
+          },
+          {
+            name: "Tree Diameter",
+            path: "/visualizer/trees/algorithms/diameter",
+          },
+          {
+            name: "Tree Isomorphism",
+            path: "/visualizer/trees/algorithms/isomorphism",
+          },
+          {
+            name: "Serialize/Deserialize",
+            path: "/visualizer/trees/algorithms/serialization",
+          },
+        ],
+      },
+      {
+        title: "Applications",
+        items: [
+          {
+            name: "Heap Sort",
+            path: "/visualizer/trees/applications/heapsort",
+          },
+          {
+            name: "Huffman Coding",
+            path: "/visualizer/trees/applications/huffman",
+          },
+          {
+            name: "Decision Trees",
+            path: "/visualizer/trees/applications/decision-trees",
+          },
+          {
+            name: "Syntax Trees",
+            path: "/visualizer/trees/applications/syntax-trees",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    title: "Graph",
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
+        />
+      </svg>
+    ),
+    subsections: [
+      {
+        title: "Representation",
+        items: [
+          {
+            name: "Adjacency Matrix",
+            path: "/visualizer/graph/representation/adjacency-matrix",
+          },
+          {
+            name: "Adjacency List",
+            path: "/visualizer/graph/representation/adjacency-list",
+          },
+        ],
+      },
+      {
+        title: "Traversal",
+        items: [
+          {
+            name: "Breadth-First Search (BFS)",
+            path: "/visualizer/graph/traversal/bfs",
+          },
+          {
+            name: "Depth-First Search (DFS)",
+            path: "/visualizer/graph/traversal/dfs",
+          },
+        ],
+      },
+      {
+        title: "Algorithms",
+        items: [
+          {
+            name: "Dijkstra's Algorithm",
+            path: "/visualizer/graph/algorithms/dijkstra",
+          },
+          {
+            name: "Prim's Algorithm",
+            path: "/visualizer/graph/algorithms/prim",
+          },
+          {
+            name: "Kruskal's Algorithm",
+            path: "/visualizer/graph/algorithms/kruskal",
+          },
+          {
+            name: "Topological Sort",
+            path: "/visualizer/graph/algorithms/topological-sort",
+          },
+        ],
+      },
+    ],
+  },
+];
 
   const Visualizer = () => {
     return (
