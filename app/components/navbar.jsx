dd8df751d2195248deaca2b7dd48e9a11b102b86
Sohan Rout <sohanrout84@gmail.com>
2025-05-31 22:27:02 +0530

    updated navbar

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -19,18 +19,6 @@ const SERVICES = [
     href: "/tutor"
   },
   {
-    title: "Practice",
-    description: "Interactive coding compiler",
-    icon: (
-      <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-        <polyline points="16 18 22 12 16 6"></polyline>
-        <polyline points="8 6 2 12 8 18"></polyline>
-      </svg>
-    ),
-    iconBg: "bg-green-100 text-green-600",
-    href: "/compiler"
-  },
-  {
     title: "Algorithm Visualizer",
     description: "Step-by-step algorithm visualization",
     icon: (
