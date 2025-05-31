4b9ed9e76ccc8d402ed176956dfa2f79c955a5d4
Sohan Rout <sohanrout84@gmail.com>
2025-05-31 22:23:59 +0530

    added circualr linekd list

--- app/visualizer/linkedList/types/circular/page.jsx
+++ app/visualizer/linkedList/types/circular/page.jsx
@@ -2,23 +2,26 @@ import Animation from "@/app/visualizer/linkedList/types/circular/animation";
 import Navbar from "@/app/components/navbar";
 
 export const metadata = {
-    title: 'Circular Linked List Visualizer | Step-by-Step CLL Animation',
-    description: 'Visualize Circular Linked List operations like insertion, deletion, and traversal in a continuous loop. Learn how CLLs work with interactive animations and improve your DSA fundamentals.',
-    keywords: [
-      'Circular Linked List Visualizer',
-      'CLL Animation',
-      'Visualize Circular Linked List',
-      'Learn Circular Linked List',
-      'Circular Linked List DSA',
-      'Cicular Linked List for Beginners',
-      'Insertion in Circular Linked List',
-      'Deletion in Circular Linked List',
-      'Circular Linked List Traversal',
-      'DSA Circular Linked List Visualization',
-    ],
-    robots: "index, follow",
-  };
-
+  title: 'Circular Linked List Algorithm | Interactive Learning & Step-by-Step Animation',
+  description:
+    'Master Circular Linked Lists with interactive visualizations, quizzes, and implementation code. Learn insertion, deletion, and traversal through animations and practice with hands-on exercises.',
+  keywords: [
+    'Circular Linked List Visualizer',
+    'CLL Animation',
+    'Visualize Circular Linked List',
+    'Learn Circular Linked List',
+    'Circular Linked List DSA',
+    'Circular Linked List for Beginners',
+    'Insertion in Circular Linked List',
+    'Deletion in Circular Linked List',
+    'Circular Linked List Traversal',
+    'DSA Circular Linked List Visualization',
+    'DSA Quiz Circular Linked List',
+    'Circular Linked List Implementation Code',
+    'DSA Learning Platform',
+  ],
+  robots: 'index, follow',
+};
 export default function Page() {
   return (
     <>
