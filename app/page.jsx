cb741c54ad69c48dc98636112761e0f7035ddbe7
Sohan Rout <sohanrout84@gmail.com>
2025-11-02 22:05:31 +0530

    Feat : first ad component added

--- app/page.jsx
+++ app/page.jsx
@@ -6,6 +6,7 @@ import Review from "@/app/components/review";
 import Testimonial from "@/app/components/testimonial";
 import Faq from "@/app/components/faq";
 import Footer from "@/app/components/footer";
+import BottomAd from "./components/ads/bottom";
 
 export const metadata = {
   title: 'DSA Visualizer | Visualize & Learn DSA the Smart Way',
@@ -42,27 +43,32 @@ export const metadata = {
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-gray-100">
-      <Navbar />
-      <div id="hero">
-        <Hero />
+    <>
+      <div className="min-h-screen bg-gray-100">
+        <Navbar />
+        <div id="hero">
+          <Hero />
+        </div>
+        <div id="testimonial">
+          <Testimonial/>
+        </div>
+        <div id="features">
+          <Feature />
+        </div>
+        <div id="about">
+          <About />
+        </div>
+        <div id="faq">
+          <Faq />
+        </div>
+        <div id="review">
+          <Review/>
+        </div>
+        <Footer />
       </div>
-      <div id="testimonial">
-        <Testimonial/>
-      </div>
-      <div id="features">
-        <Feature />
-      </div>
-      <div id="about">
-        <About />
-      </div>
-      <div id="faq">
-        <Faq />
-      </div>
-      <div id="review">
-        <Review/>
-      </div>
-      <Footer />
-    </div>
+
+      {/* Floating Ad — always visible at bottom */}
+      <BottomAd />
+    </>
   );
 }
